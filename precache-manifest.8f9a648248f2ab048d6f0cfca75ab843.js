c17715c189b431c185bcb62044f414386cf23671
kerollos0 <kerozaher888@gmail.com>
2020-09-23 18:52:34 +0200

    Updates

---
+++ precache-manifest.8f9a648248f2ab048d6f0cfca75ab843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058cb37ecf715d960b9e182b13267529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e932c2b17a90833a06",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2463053133609d23c9a9",
+    "url": "/static/css/main.3ccd4de7.chunk.css"
+  },
+  {
+    "revision": "54e932c2b17a90833a06",
+    "url": "/static/js/2.ef8672e5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ef8672e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2463053133609d23c9a9",
+    "url": "/static/js/main.945aa593.chunk.js"
+  },
+  {
+    "revision": "27e7b6d1095f4f8a0aa9",
+    "url": "/static/js/runtime-main.08c4e3d1.js"
+  }
+]);
